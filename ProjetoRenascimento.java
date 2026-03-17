f59fd7d92218236d5b996c283b5b17d4219455cc
Samuel Rodrigues Sales <4261210580@ulife.com.br>
2026-03-17 20:49:25 -0300

    first commit

---
+++ ProjetoRenascimento.java
@@ -0,0 +1,12 @@
+public class ProjetoRenascimento {
+    public static void main(String[] args) {
+        boolean possuiCoragem = true;
+        int saudeFisica = 40;
+
+        if (possuiCoragem && saudeFisica > 30) {
+            System.out.println("Você está pronto para enfrentar os desafios do Renascimento!");
+        } else {
+          System.out.println("Você precisa de mais coragem e saúde física para enfrentar os desafios do Renascimento");  
+        }
+    }
+}
